e5b56f89bd08231038ea70b87c06b4f1d0f565ee
LongDT <thelong0705@gmail.com>
2020-06-14 13:08:07 +0900

    show tooltip over selected text

--- content.js
+++ content.js
@@ -1,13 +1,21 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message  === "clicked_browser_action"){
+  function (request, sender, sendResponse) {
+    if (request.message === "clicked_browser_action") {
       var text = "";
       if (window.getSelection) {
-          text = window.getSelection().toString();
+        text = window.getSelection().toString();
       } else if (document.selection && document.selection.type != "Control") {
-          text = document.selection.createRange().text;
+        text = document.selection.createRange().text;
       }
-      chrome.runtime.sendMessage({"message": "open_new_tab", "text": text});  
+      chrome.runtime.sendMessage({ "message": "open_new_tab", "text": text });
     }
   }
+);
+
+$.tooltipster.startSelectable(
+	'html',
+	function(instance, selectedText) {
+		return selectedText;
+	},
+	{ animation: 'fade' }
 );
