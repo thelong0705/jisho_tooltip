aca0a91d8aa4858c1bc22f513c6f6b1d16e97321
LongDT <doan.the.long@moneyforward.co.jp>
2020-07-17 15:28:39 +0900

    press d to translate word

--- tooltip.js
+++ tooltip.js
@@ -11,15 +11,27 @@ $(document.body).append(`
   </div>
 `);
 
-document.onmouseup = async function () {
+document.onkeydown = function (event) {
+  if (event.keyCode == 68){ // d button's keycode 
+    showTooltip();
+  } 
+}
+
+document.onmousedown = function () {
+  $('.translate').hide();
+  $('.jisho-loading').hide();
+  $('.translated-text').html(``)
+}
+
+async function showTooltip() {
   let selection = document.getSelection();
   let selectionString = selection.toString().trim();
   if (selectionString) {
     let browserWidth = $(window).width();
     let { x, y, width, height } = selection.getRangeAt(0).getBoundingClientRect()
     console.log(x, y, width, height)
-    let translateLeftPosition = Math.max(x + width/2 - 15, 0); 
-    let translateRightPosition = Math.max(browserWidth - translateLeftPosition - 30, 0); 
+    let translateLeftPosition = Math.max(x + width / 2 - 15, 0);
+    let translateRightPosition = Math.max(browserWidth - translateLeftPosition - 30, 0);
     let scrollTop = $(window).scrollTop();
     let translateTopPosition = scrollTop + y + height + 5
 
@@ -28,7 +40,7 @@ document.onmouseup = async function () {
     $('.translate').css('right', 'auto');
     $('.jisho-arrow').css('left', 10);
     $('.jisho-arrow').css('right', 'auto')
-    
+
     let loadingWidth = $('.jisho-loading').width();
     if (translateLeftPosition + loadingWidth > browserWidth - 10) {
       $('.translate').css('right', translateRightPosition);
@@ -36,7 +48,7 @@ document.onmouseup = async function () {
       $('.jisho-arrow').css('right', 10);
       $('.jisho-arrow').css('left', 'auto')
     }
-    
+
     $('.translate').show();
     $('.jisho-loading').show();
     try {
@@ -47,7 +59,7 @@ document.onmouseup = async function () {
       $('.jisho-loading').hide();
       $('.translated-text').html(`${translated}`)
       let divWidth = $('.translate').width();
-      if(translateLeftPosition + divWidth > browserWidth){
+      if (translateLeftPosition + divWidth > browserWidth) {
         $('.translate').css('right', translateRightPosition);
         $('.translate').css('left', 'auto');
         $('.jisho-arrow').css('right', 10);
@@ -55,30 +67,6 @@ document.onmouseup = async function () {
       }
     }
   }
-};
-
-document.onmousedown = function () {
-  $('.translate').hide();
-  $('.jisho-loading').hide();
-  $('.translated-text').html(``)
-}
-
-async function translate(word) {
-  let jisho_api = "https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words?keyword="
-  let response = await fetch(jisho_api + word, {
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  });
-  let responseJson = await response.json();
-  return responseJson.data[0].senses[0].english_definitions.toString();
-}
-
-
-document.onmousedown = function () {
-  $('.translate').hide();
-  $('.jisho-loading').hide();
-  $('.translated-text').html(``)
 }
 
 async function translate(word) {
