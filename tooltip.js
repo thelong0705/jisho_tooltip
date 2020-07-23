9f1fd766bac16d5cd2ba15627d74b3ef985d70a3
LongDT <doan.the.long@moneyforward.co.jp>
2020-07-23 20:00:31 +0900

    refactored code

--- tooltip.js
+++ tooltip.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         <div></div>
         <div></div>
       </div>
+      <div class="error">There is something wrong. Check your internet connection</div>
       <div class="translate-body">
         <div class="word-not-found">Can not translate this word</div>
         <div class="japanese">
@@ -36,75 +37,92 @@ document.onmousedown = function () {
   $('.translate-body').hide();
 }
 
-async function showTooltip() {
+function findSelect() {
   let selection = document.getSelection();
   let selectionString = selection.toString().trim();
-  if (selectionString) {
-    let windowWidth = $(window).width();
-    let { x, y, width, height } = selection.getRangeAt(0).getBoundingClientRect()
-    console.log(x, y, width, height)
-    let translateLeftPosition = Math.max(x + width / 2 - 15, 0);
-    let translateRightPosition = Math.max(windowWidth - translateLeftPosition - 30, 0);
-    let scrollTop = $(window).scrollTop();
-    let translateTopPosition = scrollTop + y + height + 5
+  return { selection, selectionString };
+}
 
-    $('.translate').css('top', translateTopPosition);
-    $('.translate').css('left', translateLeftPosition);
+function getPosition(selection) {
+  let { x, y, width, height } = selection.getRangeAt(0).getBoundingClientRect();
+
+  let windowWidth = $(window).width();
+  let translateLeftPosition = Math.max(x + width / 2 - 15, 0);
+  let translateRightPosition = Math.max(windowWidth - translateLeftPosition - 30, 0);
+  let isSkewRight = translateLeftPosition + 300 > windowWidth;
+  let translateHorizontalPosition = isSkewRight ? translateRightPosition : translateLeftPosition;
+
+  let scrollTop = $(window).scrollTop();
+  let translateVerticalPosition = scrollTop + y + height + 5
+
+  return { translateVerticalPosition, translateHorizontalPosition, isSkewRight }
+}
+
+function setPosition(translateVerticalPosition, translateHorizontalPosition, isSkewRight) {
+  $('.translate').css('top', translateVerticalPosition);
+
+  if (isSkewRight) {
+    $('.translate').css('right', translateHorizontalPosition);
+    $('.translate').css('left', 'auto');
+    $('.jisho-arrow').css('right', 10);
+    $('.jisho-arrow').css('left', 'auto')
+  } else {
+    $('.translate').css('left', translateHorizontalPosition);
     $('.translate').css('right', 'auto');
     $('.jisho-arrow').css('left', 10);
     $('.jisho-arrow').css('right', 'auto')
-
-    let loadingWidth = $('.jisho-loading').width();
-    var skewRight = translateLeftPosition + 300 > windowWidth
-    if (skewRight) {
-      $('.translate').css('right', translateRightPosition);
-      $('.translate').css('left', 'auto');
-      $('.jisho-arrow').css('right', 10);
-      $('.jisho-arrow').css('left', 'auto')
-    }
-
-    $('.translate').show();
-    $('.jisho-loading').show();
-    $('.translate-body').hide();
-
-    try {
-      var translatedJson = await translate(selectionString)
-    } catch (error) {
-      translated = "Can't not connect to Internet"
-    } finally {
-      $('.jisho-loading').hide();
-      $('.translate-body').show();
-      if (!translatedJson) {
-        $(".word-not-found").show();
-        return;
-      }
-      $(".word-not-found").hide();
-      let englishHTML = ''
-      for (let index = 0; index < translatedJson.senses.length; index++) {
-        let englishDefinitions = translatedJson.senses[index].english_definitions.join(', ');
-        englishHTML += `<p>${index + 1}. ${englishDefinitions}</p>`
-      }
-      $('.english').html(`${englishHTML}`)
-      
-      if (translatedJson.japanese[0].word) {
-        $('.word').html(`${translatedJson.japanese[0].word}`);
-        $('.reading').html(`${translatedJson.japanese[0].reading}`);
-      } else {
-        $('.word').html(`${translatedJson.japanese[0].reading}`);
-        $('.reading').html(``);
-      }
-      
-      $('.furigana').html(`${translatedJson.japanese[0].reading}`)
-      $('.kanji').html(`${translatedJson.japanese[0].word}`)
-      let divWidth = $('.translate').width();
-      if (skewRight) {
-        $('.translate').css('right', translateRightPosition);
-        $('.translate').css('left', 'auto');
-        $('.jisho-arrow').css('right', 10);
-        $('.jisho-arrow').css('left', 'auto')
-      }
-    }
   }
+}
+
+function showLoading() {
+  $('.translate').show();
+  $('.jisho-loading').show();
+  $('.translate-body').hide();
+  $('.error').hide();
+}
+
+function showTranslated(translatedJson) {
+  $('.jisho-loading').hide();
+  $('.translate-body').show();
+  if (!translatedJson) {
+    $(".word-not-found").show();
+    return;
+  }
+  $(".word-not-found").hide();
+  let englishHTML = ''
+  for (let index = 0; index < translatedJson.senses.length; index++) {
+    let englishDefinitions = translatedJson.senses[index].english_definitions.join(', ');
+    englishHTML += `<p>${index + 1}. ${englishDefinitions}</p>`
+  }
+  $('.english').html(`${englishHTML}`);
+
+  if (translatedJson.japanese[0].word) {
+    $('.word').html(`${translatedJson.japanese[0].word}`);
+    $('.reading').html(`${translatedJson.japanese[0].reading}`);
+  } else {
+    $('.word').html(`${translatedJson.japanese[0].reading}`);
+    $('.reading').html(``);
+  }
+}
+
+async function showTooltip() {
+  const { selection, selectionString } = findSelect();
+  if (!selectionString) return;
+  const { translateVerticalPosition, translateHorizontalPosition, isSkewRight } = getPosition(selection);
+  setPosition(translateVerticalPosition, translateHorizontalPosition, isSkewRight)
+  showLoading();
+  
+  let translatedJson;
+  
+  try {
+    translatedJson = await translate(selectionString)
+  } catch (error) {
+    $('.jisho-loading').hide();
+    $('.translate-body').hide();
+    $('.error').show();
+    return;
+  }
+  showTranslated(translatedJson)
 }
 
 async function translate(word) {
