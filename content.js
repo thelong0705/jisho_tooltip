03092f6603f480b3e00f623115bc3cb36281ff4a
LongDT <doan.the.long@moneyforward.co.jp>
2020-07-25 22:53:27 +0900

    remove unnecessary code

--- content.js
+++ content.js
@@ -1,17 +1,3 @@
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "clicked_browser_action") {
-      var text = "";
-      if (window.getSelection) {
-        text = window.getSelection().toString();
-      } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-      }
-      chrome.runtime.sendMessage({ "message": "open_new_tab", "text": text });
-    }
-  }
-);
-
 $(document).ready(function () {
   $(document.body).append(`
     <div class="translate">
