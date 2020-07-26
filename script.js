0dc260540d29dc9441442d2ddb14809fb1b05830
LongDT <doan.the.long@moneyforward.co.jp>
2020-07-26 17:49:15 +0900

    refactored code and HTML class name

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 $(document).ready(function () {
-  $(document.body).append(`
-    <div class="translate">
-      <div class="jisho-arrow">
-        <div class="jisho-arrow-inside"></div>
-      </div>
-      <div class="jisho-loading">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="error">There is something wrong. Check your internet connection</div>
-      <div class="translate-body">
-        <div class="word-not-found">Can not translate this word</div>
-        <div class="japanese">
-          <div class="reading"></div>
-          <div class="word"></div>
-          <hr>
-        </div>
-        <div class="english">
-        </div>
-      </div>
-    </div>
-`);
+  Tooltip.appendHTML();
 });
 
 document.onkeydown = function (event) {
@@ -31,63 +9,16 @@ document.onkeydown = function (event) {
 }
 
 document.onmousedown = function () {
-  $('.translate').hide();
-  $('.jisho-loading').hide();
-  $('.translate-body').hide();
-}
-
-function showTranslated(translatedJson) {
-  $('.jisho-loading').hide();
-  $('.translate-body').show();
-  if (!translatedJson) {
-    $(".word-not-found").show();
-    return;
-  }
-  $(".word-not-found").hide();
-  let englishHTML = ''
-  for (let index = 0; index < translatedJson.senses.length; index++) {
-    let englishDefinitions = translatedJson.senses[index].english_definitions.join(', ');
-    englishHTML += `<p>${index + 1}. ${englishDefinitions}</p>`
-  }
-  $('.english').html(`${englishHTML}`);
-
-  if (translatedJson.japanese[0].word) {
-    $('.word').html(`${translatedJson.japanese[0].word}`);
-    $('.reading').html(`${translatedJson.japanese[0].reading}`);
-  } else {
-    $('.word').html(`${translatedJson.japanese[0].reading}`);
-    $('.reading').html(``);
-  }
+  Tooltip.hide();
 }
 
 async function showTooltip() {
   let selection = document.getSelection();
   let tooltip = new Tooltip(selection);
   if (!tooltip.selectionString) return;
-  tooltip.setTooltipCSS();
-  tooltip.setArrowCSS();
+  Tooltip.setTooltipPosition(tooltip);
+  Tooltip.setTooltipArrowPosition(tooltip);
   Tooltip.showLoading();
-  
-  let translatedJson;
-  
-  try {
-    translatedJson = await translate(tooltip.selectionString)
-  } catch (error) {
-    $('.jisho-loading').hide();
-    $('.translate-body').hide();
-    $('.error').show();
-    throw error;
-  }
-  showTranslated(translatedJson)
-}
-
-async function translate(word) {
-  let jisho_api = "https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words?keyword="
-  let response = await fetch(jisho_api + word, {
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  });
-  let responseJson = await response.json();
-  return responseJson.data?.[0];
+  await tooltip.setTooltipContent();
+  Tooltip.showBody(tooltip)
 }
--- tooltip.js
+++ tooltip.js
@@ -2,73 +2,205 @@ class Tooltip {
   constructor(selection) {
     this.selection = selection;
     this.selectionString = selection.toString().trim();
-    if(this.selectionString){
+    if (this.selectionString) {
       this.arrowHorizontalRelativeDistance = 10;
-      this.tooltipHorizontalPostion = this.centerArrowInsideSelection();
-      this.tooltipVerticalPosition = this.placeTooltipUnderSelection();
-      this.isArrowInTheLeftSide = this.tooltipHorizontalPostion.right === 'auto';  
+      this.tooltipHorizontalPostion = this.getTooltipHorizontalPosition();
+      this.tooltipVerticalPosition = this.getTooltipVerticalPosition();
+      this.isArrowInTheLeftSide = this.tooltipHorizontalPostion.right === 'auto';
     }
   }
 
-  getSelectionBoundingRectangle(){
+  getSelectionBoundingRectangle() {
     let { x, y, width, height } = this.selection.getRangeAt(0).getBoundingClientRect();
-    
+
     //handle highlightext span mulitple lines
     let selectionParentElement = this.selection.anchorNode.parentElement;
     let fontSize = parseInt(
       window.getComputedStyle(selectionParentElement, null)
         .getPropertyValue('font-size')
     );
-    
+
     if (height > fontSize * 2) { // check span multiple line
       let range = new Range();
-      range.setStart(selectionParentElement.firstChild, this.selection.focusOffset - 1);
+
+      range.setStart(selectionParentElement.firstChild,
+        this.selection.focusOffset - 1);
       range.setEnd(selectionParentElement.firstChild, this.selection.focusOffset);
       width = range.getBoundingClientRect().right - $(selectionParentElement).offset().left;
     }
     return { x, y, width, height }
   }
 
-  
-  centerArrowInsideSelection() {
+
+  getTooltipHorizontalPosition() {
     let { x, _y, width, height } = this.getSelectionBoundingRectangle();
     let windowWidth = $(window).width();
-    let arrowWidth = parseInt($('.jisho-arrow').css('borderLeft')) + parseInt($('.jisho-arrow').css('borderRight'));
+    let arrowWidth = parseInt(Tooltip.$tooltipArrow.css('borderLeft')) + parseInt(Tooltip.$tooltipArrow.css('borderRight'));
     let arrowChopToTooltipDistance = this.arrowHorizontalRelativeDistance + arrowWidth / 2;
     let tooltipLeftPosition = Math.max(x + width / 2 - arrowChopToTooltipDistance, 0);
     let tooltipRightPosition = Math.max(windowWidth - tooltipLeftPosition - arrowChopToTooltipDistance * 2, 0);
-    let tooltipMaxWidth = parseInt($('.translate').css('maxWidth'));
+    let tooltipMaxWidth = parseInt(Tooltip.$tooltip.css('maxWidth'));
 
     if (tooltipLeftPosition + tooltipMaxWidth < windowWidth) {
-      return { 'left': tooltipLeftPosition, 'right': 'auto' }
+      return {
+        left: tooltipLeftPosition,
+        right: 'auto'
+      }
     }
-    return { 'left': 'auto', 'right': tooltipRightPosition }
+    return {
+      left: 'auto',
+      right: tooltipRightPosition
+    }
   }
 
-  placeTooltipUnderSelection() {
+  getTooltipVerticalPosition() {
     let { _x, y, _width, height } = this.selection.getRangeAt(0).getBoundingClientRect();
-    let arrowHeight = parseInt($('.jisho-arrow').css('borderBottom'))
+    let arrowHeight = parseInt(Tooltip.$tooltipArrow.css('borderBottom'))
     let tooltipTopPosition = $(window).scrollTop() + y + height + arrowHeight;
-    return { 'top': tooltipTopPosition, 'bottom': 'auto' }
-  }
-
-  setTooltipCSS() {
-    $('.translate').css(this.tooltipVerticalPosition);
-    $('.translate').css(this.tooltipHorizontalPostion);
-  }
-
-  setArrowCSS() {
-    let arrowPostiton = { 'left': this.arrowHorizontalRelativeDistance, right: 'auto' };
-    if (!this.isArrowInTheLeftSide) {
-      arrowPostiton = { 'left': 'auto', right: this.arrowHorizontalRelativeDistance }
+    return {
+      top: tooltipTopPosition,
+      bottom: 'auto'
     }
-    $('.jisho-arrow').css(arrowPostiton);
+  }
+
+  async getTranslatedJson() {
+    let cors_url = "https://cors-anywhere.herokuapp.com/";
+    let jisho_api = "https://jisho.org/api/v1/search/words?keyword=";
+    let fetch_url = cors_url + jisho_api + this.selectionString;
+    let response = await fetch(fetch_url, {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    });
+    let responseJson = await response.json();
+    return responseJson.data?.[0];
+  }
+
+  async setTooltipContent() {
+    let translatedJson;
+    try {
+      translatedJson = await this.getTranslatedJson();
+    } catch (error) {
+      this.error = "There is something wrong! Check your internet connection";
+      return;
+    }
+
+    if (!translatedJson) {
+      this.wordNotFound = "Can't look up this word"
+      return;
+    }
+
+    let englishHTML = '<hr>'
+    for (let index = 0; index < translatedJson.senses.length; index++) {
+      let englishDefinitions = translatedJson.senses[index].english_definitions.join(', ');
+      englishHTML += `<p>${index + 1}. ${englishDefinitions}</p>`
+    }
+
+    this.englishHTML = englishHTML;
+    this.wordHTML = translatedJson.japanese[0].word;
+    this.readingHTML = translatedJson.japanese[0].reading;
   }
 }
 
-Tooltip.showLoading = () => {
-  $('.translate').show();
-  $('.jisho-loading').show();
-  $('.translate-body').hide();
-  $('.error').hide();
+Tooltip.html = `
+<div class="jisho-tooltip">
+  <div class="jisho-tooltip-arrow">
+    <div class="jisho-tooltip-arrow-inside"></div>
+  </div>
+
+  <div class="jisho-tooltip-loading">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+ 
+  <div class="jisho-tooltip-body">
+    <div class="error"></div>
+    <div class="word-not-found"></div>
+    <div class="japanese">
+      <div class="reading"></div>
+      <div class="word"></div>
+    </div>
+    <div class="english">
+    </div>
+  </div>
+</div>
+`
+
+Tooltip.appendHTML = function () {
+  $(document.body).append(this.html);
+  Object.assign(Tooltip, {
+    $tooltip: $('.jisho-tooltip'),
+    $tooltipArrow: $('.jisho-tooltip .jisho-tooltip-arrow'),
+    $tooltipLoading: $('.jisho-tooltip .jisho-tooltip-loading'),
+    $tooltipBody: $('.jisho-tooltip .jisho-tooltip-body'),
+    $tooltipBodyJapaneseReading: $('.jisho-tooltip .jisho-tooltip-body .japanese .reading'),
+    $tooltipBodyJapaneseWord: $('.jisho-tooltip .jisho-tooltip-body .japanese .word'),
+    $tooltipBodyEnglish: $('.jisho-tooltip .jisho-tooltip-body .english'),
+    $tooltipBodyError: $('.jisho-tooltip .jisho-tooltip-body .error'),
+    $tooltipBodyWordNotFound: $('.jisho-tooltip .jisho-tooltip-body .word-not-found'),
+  })
+  this.$tooltip.hide();
+}
+
+Tooltip.showLoading = function () {
+  this.$tooltip.show();
+  this.$tooltipLoading.show();
+  this.$tooltipBody.hide();
+}
+
+Tooltip.hideLoading = function () {
+  this.$tooltipLoading.hide();
+}
+
+Tooltip.hide = function () {
+  this.$tooltip.hide();
+}
+
+Tooltip.resetTooltipBody = function () {
+  this.$tooltipBodyJapaneseReading.html('');
+  this.$tooltipBodyJapaneseWord.html('');
+  this.$tooltipBodyEnglish.html('');
+  this.$tooltipBodyError.html('');
+  this.$tooltipBodyWordNotFound.html('');
+}
+
+Tooltip.setTooltipBodyHTML = function (tooltip) {
+  this.$tooltipBodyError.html(tooltip.error);
+  this.$tooltipBodyWordNotFound.html(tooltip.wordNotFound);
+  this.$tooltipBodyEnglish.html(tooltip.englishHTML);
+
+  if (tooltip.wordHTML) {
+    this.$tooltipBodyJapaneseWord.html(tooltip.wordHTML);
+    this.$tooltipBodyJapaneseReading.html(tooltip.readingHTML);
+  } else {
+    this.$tooltipBodyJapaneseWord.html(tooltip.readingHTML);
+    this.$tooltipBodyJapaneseReading.html('');
+  }
+}
+
+Tooltip.showBody = function (tooltip) {
+  this.hideLoading();
+  this.resetTooltipBody();
+  this.setTooltipBodyHTML(tooltip)
+  this.$tooltipBody.show();
+}
+
+Tooltip.setTooltipPosition = function (tooltip) {
+  this.$tooltip.css(tooltip.tooltipVerticalPosition);
+  this.$tooltip.css(tooltip.tooltipHorizontalPostion);
+}
+
+Tooltip.setTooltipArrowPosition = function (tooltip) {
+  let arrowPostiton = {
+    left: tooltip.arrowHorizontalRelativeDistance,
+    right: 'auto'
+  };
+  if (!tooltip.isArrowInTheLeftSide) {
+    arrowPostiton = {
+      left: 'auto',
+      right: tooltip.arrowHorizontalRelativeDistance
+    }
+  }
+  this.$tooltipArrow.css(arrowPostiton);
 }
